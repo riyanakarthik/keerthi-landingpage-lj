518bec519a645539b2eeccbea9c4683e79d3b8e1
riyanakarthik <166106699+riyanakarthik@users.noreply.github.com>
2025-07-23 14:04:56 +0530

    Make layout responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className="m-0 mx-auto max-w-7xl">{children}</body>
+      <body className="m-0 mx-auto w-full">{children}</body>
     </html>
   )
 }
